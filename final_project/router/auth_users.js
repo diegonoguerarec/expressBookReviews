bd5d38cd0c99e6d4ddee2934a0d5397e6f5c82f3
Diegao <diegonoguerarec@gmail.com>
2026-01-21 20:13:10 -0300

    Task 7 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,13 +10,42 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  //write code to check if username and password match the one we have in records.
+  let userList = users.filter((user) => user.username === username && user.password === password);
+
+  if (userList.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let user = req.body.username;
+  let pass = req.body.password;
+
+  // Check if username and password are missing
+  if (!user || !pass) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(user, pass)) {
+    // Generate JWT
+    let accessToken = jwt.sign({data: pass}, "access", {expiresIn: 60*60});
+
+    // Store accessToken and username un session
+    req.session.authorization = {
+      accessToken,
+      user,
+    };
+
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
+
 });
 
 // Add a book review
