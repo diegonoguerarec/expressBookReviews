cd902d95d7f1841cca514a55b7a3375b38fc309b
Diegao <diegonoguerarec@gmail.com>
2026-01-21 20:41:19 -0300

    Task 9 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -72,13 +72,31 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   if (!review) {
     return res.status(400).json({message: "Review is required"});
   }
-  
+
   // Add review to book
   book["reviews"][username] = review;
 
   return res.status(200).json({
     message: "Review successfully added/updated",
     reviews: books[isbn].reviews
+  });
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  let isbn = req.params.isbn;
+  let username = req.session.authorization.user;
+  let book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  // Delete review
+  delete book["reviews"][username];
+
+  return res.status(200).json({
+    message: "Review successfully deleted",
   });
 });
 
