ca7b8cef91a70d4e06fba4e14b9938ebc6ad6009
Diegao <diegonoguerarec@gmail.com>
2026-01-21 19:26:59 -0300

    Task 2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,9 +17,15 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+
+  if (book) {
+    res.send(JSON.stringify(book, null, 4));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
